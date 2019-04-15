325994b21263e020a948d714c446cc3928be5538
Brandon Jenkins <jenkinsb@sonoma.edu>
2019-04-15 16:59:08 -0700

    Downloaded the api

---
+++ app/src/main/java/AppModels/JSON.java
@@ -0,0 +1,20 @@
+package AppModels;
+
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
+
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLEncoder;
+
+
+public class JSON
+{
+    private static JsonElement json;
+
+    public JSON()
+    {
+    }
+}
+
