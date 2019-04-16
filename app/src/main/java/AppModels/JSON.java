d7d786e01731cb028edea3891bf180e81adb2a63
Brandon Jenkins <jenkinsb@sonoma.edu>
2019-04-15 17:19:51 -0700

    Made the JSON class
    Made the json class if we decide to take the wifi route. I am not sure how the different calls or indexing through the JSON will work yet

--- app/src/main/java/AppModels/JSON.java
+++ app/src/main/java/AppModels/JSON.java
@@ -3,10 +3,11 @@ package AppModels;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonParser;
 
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.net.MalformedURLException;
 import java.net.URL;
-import java.net.URLEncoder;
 
 
 public class JSON
@@ -15,6 +16,41 @@ public class JSON
 
     public JSON()
     {
+    }
+
+    public static JsonElement getData(String search)
+    {
+        try
+        {
+            //Construct the API url
+            String apiURL = "http://dnd5eapi.co/api/" + search;
+
+            //Create URL object
+            URL DndURL = new URL(apiURL);
+
+            //Create IS Object
+            InputStream is = DndURL.openStream();
+
+            //Create ISR
+            InputStreamReader isr = new InputStreamReader(is);
+
+            //Parse through the input stream into JsonElement
+            JsonParser parser = new JsonParser();
+
+            json = parser.parse(isr);
+        }
+
+        catch (MalformedURLException mue)
+        {
+            System.out.println("Malformed URL");
+        }
+
+        catch (IOException e)
+        {
+            System.out.println("IO Error");
+        }
+
+        return json;
     }
 }
 
