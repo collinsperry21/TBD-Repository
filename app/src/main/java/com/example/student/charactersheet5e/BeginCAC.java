c867de4d5565ebd857f0a88e08de1c9aa1138231
Brandon Jenkins <jenkinsb@sonoma.edu>
2019-04-17 17:19:36 -0700

    Added spinners pulling form API

--- app/src/main/java/com/example/student/charactersheet5e/BeginCAC.java
+++ app/src/main/java/com/example/student/charactersheet5e/BeginCAC.java
@@ -4,17 +4,38 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+import static com.android.volley.toolbox.Volley.newRequestQueue;
+
 
 public class BeginCAC extends AppCompatActivity {
 
     private Button navigate_next_CAC;
+    private RequestQueue mQueue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_begin_cac);
+
+
 
         navigate_next_CAC = findViewById(R.id.NextCAC01);
         navigate_next_CAC.setOnClickListener(new View.OnClickListener()
@@ -26,5 +47,159 @@ public class BeginCAC extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        mQueue = newRequestQueue( this);
+        jsonParse();
+    }
+
+    //Will move to separate class
+    private void jsonParse() {
+        String url = "http://www.dnd5eapi.co/api/races";
+        String url2 = "http://dnd5eapi.co/api/subraces";
+        String url3 = "http://dnd5eapi.co/api/classes";
+        String url4 = "http://dnd5eapi.co/api/subclasses";
+
+
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url, null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            ArrayList<String> raceSpinnerArrayList = new ArrayList<String>();
+
+                            JSONArray jsonArray = response.getJSONArray("results");
+                            for (int i = 0; i < jsonArray.length(); i++) {
+                                JSONObject races = jsonArray.getJSONObject(i);
+                                String firstname = races.getString("name");
+                                String urlz = races.getString("url");
+
+                                raceSpinnerArrayList.add(firstname);
+
+                            }
+
+                            Spinner raceSpinner = (Spinner) findViewById(R.id.race_spinner);
+
+                            ArrayAdapter<String> adapter =
+                                    new ArrayAdapter<String>(getApplicationContext(),  android.R.layout.simple_spinner_dropdown_item, raceSpinnerArrayList);
+                            adapter.setDropDownViewResource( android.R.layout.simple_spinner_dropdown_item);
+
+                            raceSpinner.setAdapter(adapter);
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                error.printStackTrace();
+            }
+        });
+        JsonObjectRequest request2 = new JsonObjectRequest(Request.Method.GET, url2, null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+
+                            ArrayList<String> subraceSpinnerArrayList = new ArrayList<String>();
+                            subraceSpinnerArrayList.add("None");
+
+                            JSONArray jsonArray = response.getJSONArray("results");
+                            for (int i = 0; i < jsonArray.length(); i++) {
+                                JSONObject classes = jsonArray.getJSONObject(i);
+                                String firstname = classes.getString("name");
+                                String urlz = classes.getString("url");
+
+                                subraceSpinnerArrayList.add(firstname);
+
+                            }
+                            Spinner subraceSpinner = (Spinner) findViewById(R.id.subrace_spinner);
+
+                            ArrayAdapter<String> adapter =
+                                    new ArrayAdapter<String>(getApplicationContext(),  android.R.layout.simple_spinner_dropdown_item, subraceSpinnerArrayList);
+                            adapter.setDropDownViewResource( android.R.layout.simple_spinner_dropdown_item);
+
+                            subraceSpinner.setAdapter(adapter);
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                error.printStackTrace();
+            }
+        });
+        JsonObjectRequest request3 = new JsonObjectRequest(Request.Method.GET, url3, null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            ArrayList<String> classSpinnerArrayList = new ArrayList<String>();
+                            JSONArray jsonArray = response.getJSONArray("results");
+                            for (int i = 0; i < jsonArray.length(); i++) {
+                                JSONObject classes = jsonArray.getJSONObject(i);
+                                String firstname = classes.getString("name");
+                                String urlz = classes.getString("url");
+
+                                classSpinnerArrayList.add(firstname);
+
+                            }
+                            Spinner classSpinner = (Spinner) findViewById(R.id.class_spinner);
+
+                            ArrayAdapter<String> adapter =
+                                    new ArrayAdapter<String>(getApplicationContext(),  android.R.layout.simple_spinner_dropdown_item, classSpinnerArrayList);
+                            adapter.setDropDownViewResource( android.R.layout.simple_spinner_dropdown_item);
+
+                            classSpinner.setAdapter(adapter);
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                error.printStackTrace();
+            }
+        });
+
+        JsonObjectRequest request4 = new JsonObjectRequest(Request.Method.GET, url4, null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            ArrayList<String> subclassSpinnerArrayList = new ArrayList<String>();
+                            subclassSpinnerArrayList.add("None");
+
+                            JSONArray jsonArray = response.getJSONArray("results");
+                            for (int i = 0; i < jsonArray.length(); i++) {
+                                JSONObject classes = jsonArray.getJSONObject(i);
+                                String firstname = classes.getString("name");
+                                String urlz = classes.getString("url");
+
+                                subclassSpinnerArrayList.add(firstname);
+
+                            }
+                            Spinner subclassSpinner = (Spinner) findViewById(R.id.subclass_spinner);
+
+                            ArrayAdapter<String> adapter =
+                                    new ArrayAdapter<String>(getApplicationContext(),  android.R.layout.simple_spinner_dropdown_item, subclassSpinnerArrayList);
+                            adapter.setDropDownViewResource( android.R.layout.simple_spinner_dropdown_item);
+
+                            subclassSpinner.setAdapter(adapter);
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                error.printStackTrace();
+            }
+        });
+        mQueue.add(request);
+        mQueue.add(request2);
+        mQueue.add(request3);
+        mQueue.add(request4);
     }
 }
