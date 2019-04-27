23a5e9151368846572525819bc716df7a05b8d8f
Brandon Jenkins <jenkinsb@sonoma.edu>
2019-04-27 00:50:37 -0700

    Added text fields

--- app/src/main/java/com/example/student/charactersheet5e/DescCAC.java
+++ app/src/main/java/com/example/student/charactersheet5e/DescCAC.java
@@ -2,12 +2,25 @@ package com.example.student.charactersheet5e;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import AppModels.CharSheet;
+import AppModels.CharacterCardView;
 
 public class DescCAC extends AppCompatActivity {
+
+    private TextView name;
+    private CharSheet charSheet;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_description_cac);
+
+        //Set a new character sheet from the old one ( may be a better way to do this?? )
+        charSheet = (CharSheet) (getIntent().getSerializableExtra("characterSheet"));
+
+        name = findViewById(R.id.character_name_text2);
+        name.setText(charSheet.getCharacterName());
     }
 }
--- app/src/main/java/com/example/student/charactersheet5e/ProficienciesCAC.java
+++ app/src/main/java/com/example/student/charactersheet5e/ProficienciesCAC.java
@@ -4,12 +4,14 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import AppModels.CharSheet;
 
 public class ProficienciesCAC extends AppCompatActivity {
     private CharSheet charSheet;
+    private ImageButton navigate_to_next;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -25,6 +27,8 @@ public class ProficienciesCAC extends AppCompatActivity {
         TextView proficiencyBonusText = findViewById(R.id.proficiencyBonus_Text);
         TextView proficiencyDescText = findViewById(R.id.proficiencies_desc_text);
 
+        navigate_to_next = findViewById(R.id.nextCAC);
+
         //Assuming lvl 1 set proficiency Bonus
         proficiencyBonusText.setText("+2");
         //TODO: Save proficiency bonus
@@ -36,8 +40,7 @@ public class ProficienciesCAC extends AppCompatActivity {
             @Override
             public void onClick(View view)
             {
-                Intent intent = new Intent(ProficienciesCAC.this, );
-
+                Intent intent = new Intent(ProficienciesCAC.this, DescCAC.class);
 
                 //send the character sheet to the next activity to add scores
                 intent.putExtra("characterSheet", charSheet);
