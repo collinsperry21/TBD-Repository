aad26f124eb4fe75c2676c852a2f40f397844ed3
Brandon Jenkins <jenkinsb@sonoma.edu>
2019-04-29 00:14:45 -0700

    Added activity

---
+++ app/src/main/java/com/example/student/charactersheet5e/CharacterSheetPage1.java
@@ -0,0 +1,13 @@
+package com.example.student.charactersheet5e;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CharacterSheetPage1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.character_sheet_page_1);
+    }
+}
