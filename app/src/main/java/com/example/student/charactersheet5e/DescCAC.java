22d2ed8a0b14c101b2321776735cc75a566d775c
Brandon Jenkins <jenkinsb@sonoma.edu>
2019-04-29 11:03:08 -0700

    Loaded Spinners

--- app/src/main/java/com/example/student/charactersheet5e/DescCAC.java
+++ app/src/main/java/com/example/student/charactersheet5e/DescCAC.java
@@ -2,7 +2,11 @@ package com.example.student.charactersheet5e;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 import android.widget.TextView;
+
+import java.util.ArrayList;
 
 import AppModels.CharSheet;
 import AppModels.CharacterCardView;
@@ -11,6 +15,9 @@ public class DescCAC extends AppCompatActivity {
 
     private TextView name;
     private CharSheet charSheet;
+
+    private Spinner lawfulSpinner;
+    private Spinner goodSpinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,5 +29,35 @@ public class DescCAC extends AppCompatActivity {
 
         name = findViewById(R.id.character_name_text2);
         name.setText(charSheet.getCharacterName());
+
+        setUpSpinners();
+    }
+
+    private void setUpSpinners() {
+        ArrayList<String> lawfulList = new ArrayList<>();
+        ArrayList<String> goodList = new ArrayList<>();
+
+        lawfulList.add("Lawful");
+        lawfulList.add("Neutral");
+        lawfulList.add("Chaotic");
+
+        goodList.add("Good");
+        goodList.add("Neutral");
+        goodList.add("Evil");
+
+        ArrayAdapter<String> lawfulAdapter = new ArrayAdapter<String>(getApplicationContext(),
+                android.R.layout.simple_spinner_dropdown_item, lawfulList);
+        lawfulAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        ArrayAdapter<String> goodAdapter = new ArrayAdapter<String>(getApplicationContext(),
+                android.R.layout.simple_spinner_dropdown_item, goodList);
+        goodAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        Spinner lawfulSpinner = (Spinner) findViewById(R.id.law_spinner);
+        lawfulSpinner.setAdapter(lawfulAdapter);
+
+        Spinner goodSpinner = (Spinner) findViewById(R.id.good_spinner);
+        goodSpinner.setAdapter(goodAdapter);
+
     }
 }
