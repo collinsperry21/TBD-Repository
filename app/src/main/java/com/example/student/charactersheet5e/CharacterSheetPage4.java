11cd5530632427d50e2c6a60d7b7a3f69aa6f0ee
Collin Sperry <42603366+Vatos2@users.noreply.github.com>
2019-05-01 20:31:09 -0700

    Create CharacterSheetPage4.java

---
+++ app/src/main/java/com/example/student/charactersheet5e/CharacterSheetPage4.java
@@ -0,0 +1,20 @@
+package com.example.student.charactersheet5e;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class CharacterSheetPage4 extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        super.onCreateView(inflater, container, savedInstanceState);
+        View rootView = inflater.inflate(R.layout.character_sheet_page_3, container, false);
+
+        return rootView;
+    }
+}
